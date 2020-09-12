2576da2159513c34f7dcdde64f63a23f784a21ff
akb <doge.wow.so.scare@gmail.com>
2020-09-12 12:48:59 +0900

    feat: Filter the Oaza-level polygons from the shapefile df

--- parse_pref_ages.py
+++ parse_pref_ages.py
@@ -16,8 +16,18 @@ def csv2df(csv_path, skiprows=0, encoding="utf-8"):
     return df
 
 
-df_ages = csv2df(ages_path, skiprows=5, encoding="shift_jis")
-df_shapes = csv2df(shapes_path)
+def group_oaza_shapes(df_shapes):
+    '''Create the group of polygons for every Oaza'''
+    oazas = df_ages[df_ages["地域識別番号"] == 2]
+    print(oazas["大字・町名"].head(20))
 
-print(df_ages["大字・町名"].head())
-print(df_shapes["S_NAME"].head())
+
+if __name__ == "__main__":
+    df_ages = csv2df(ages_path, skiprows=5, encoding="shift_jis")
+    df_shapes = csv2df(shapes_path)
+
+    # Census data
+    group_oaza_shapes(df_shapes)
+
+    # Shape data
+    # print(df_shapes["S_NAME"].head())
